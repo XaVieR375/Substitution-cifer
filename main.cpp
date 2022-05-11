b77dac0caefe7d05a6bb77b9f5d407d0806adcef
XaVieR375 <67128892+XaVieR375@users.noreply.github.com>
2022-05-11 13:07:46 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,14 +26,7 @@ using std::wfstream;
 
 // Globals variables (bad practice, but alternative options like reference_wrapper seams like would create even more overhead)
 const int max_symbols_size{88 + 2};
-wchar_t key_symbols_original_wchar[max_symbols_size]
-{
-    1, 2, 3, 4, 5, 6, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30,
-    31, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138,
-    139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155,
-    156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172,
-    173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186
-}; // 88
+
 wchar_t key_symbols_original_unicode[max_symbols_size]
 {
     0x263A, 0x263B, 0x2665, 0x2666, 0x2663, 0x2660, 0x2642, 0x2640, 0x266B, 0x263C, 0x25BA,
@@ -44,16 +37,14 @@ wchar_t key_symbols_original_unicode[max_symbols_size]
     0xF1, 0xD1, 0xAA, 0xBA, 0xBF, 0x2310, 0xAC, 0xBD, 0xBC, 0xA1, 0xAB, 0xBB, 0x2591, 0x2592,
     0x2593, 0x2502, 0x2524, 0x2561, 0x2562, 0x2556, 0x2555, 0x2563, 0x2551
 }; // 88
-wchar_t key_symbols_shuffled_wchar[max_symbols_size]{};
 wchar_t key_symbols_shuffled_unicode[max_symbols_size]{};
 
 const int max_msg_size{5000};
-wchar_t users_msg_subbed_wchar[max_msg_size]{};
 wchar_t users_msg_subbed_unicode[max_msg_size];
 
 void KeyShuffled(int a_plain_size);
 void Substitute(string &msg, wchar_t msg_Subbed[], size_t msg_Subbed_size,  wchar_t k_shuffled[], string a_plain);
-void BrowseToFile(string usr_msg_plain, string a_plain, wchar_t a_shuff[], bool IO = true);
+void BrowseToFile(string usr_msg_plain, string a_plain, bool IO = true);
 
 
 int main()
@@ -62,9 +53,7 @@ int main()
     string alphabet_plain {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,./?\"\';[]1234567890!@#$%^&*()-=_+ \n\t"};//88
     string alphabet_plain_display {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,./?\"\';[]1234567890!@#$%^&*()-=_+ \\n\\t"};
 
-    size_t users_msg_size;
     string users_msg;
-    bool OpnOrSave = true;
     
     while(true)
     {
@@ -84,15 +73,16 @@ int main()
             cout << "Alphabet Plain:\t[ " << alphabet_plain_display << " ]\n";
             KeyShuffled(alphabet_plain.size());
             _setmode(_fileno(stdout), _O_U16TEXT);
-            //wcout << "Ky/Sym unicode(org):\t[ " << key_symbols_original_unicode << " ]\n";
             wcout << "Key (symbols):\t[ " << key_symbols_shuffled_unicode << " ]\n\n";
             _setmode(_fileno(stdout), _O_TEXT);
             
             cout << "Enter your message (end it with a tilde `):\n\n";
             getline(cin, users_msg, '`');
             cout << "\n\nYou're subbed message:\n\n";
-            Substitute(users_msg, users_msg_subbed_wchar, users_msg_size, key_symbols_shuffled_wchar, alphabet_plain);
-            wcout << users_msg_subbed_wchar << "\n\n";
+            Substitute(users_msg, users_msg_subbed_unicode, 0, key_symbols_shuffled_unicode, alphabet_plain);
+             _setmode(_fileno(stdout), _O_U16TEXT);
+            wcout << users_msg_subbed_unicode << "\n\n";
+            _setmode(_fileno(stdout), _O_TEXT);
             cout << "Would you like to save the the message to a txt file (y/n): ";
             users_choice = cin.get();
             while(cin.get(users_choice) && users_choice != 'y' && users_choice != 'Y' && users_choice != 'n' && users_choice != 'N')
@@ -104,30 +94,30 @@ int main()
             
             if(users_choice == 'y' || users_choice == 'Y')
             {
-                users_msg_size = users_msg.size();
-                BrowseToFile(users_msg, alphabet_plain, key_symbols_shuffled_unicode, !OpnOrSave);
+                BrowseToFile(users_msg, alphabet_plain, false);
                 getline(cin, users_msg);
             }
         }
         else if(users_selection == "2")
         {
-            BrowseToFile(users_msg, alphabet_plain, key_symbols_shuffled_unicode, OpnOrSave);
+            BrowseToFile(users_msg, alphabet_plain);
             cout << "The original message is:\n\n";
-            users_msg_size = sizeof(users_msg_subbed_unicode) / sizeof(users_msg_subbed_unicode[0]);
-            Substitute(users_msg, users_msg_subbed_unicode, users_msg_size, key_symbols_shuffled_unicode, alphabet_plain);
+            Substitute(users_msg, users_msg_subbed_unicode, max_msg_size, key_symbols_shuffled_unicode, alphabet_plain);
             cout << users_msg;
         }
         
         else if(users_selection == "3")
         {
+            wchar_t key_symbols_shuffled_wchar[max_symbols_size]{};
+            wchar_t users_msg_subbed_wchar[max_msg_size]{};
+            
             cout << "Paste in the shuffled/subbed characters:\t";
             wcin.getline(key_symbols_shuffled_wchar, max_msg_size);
             cout << "\nPaste in your message: (max 5000 chars):\n\n";
             wcin.getline(users_msg_subbed_wchar, max_msg_size);
             
             cout << "\n\nThe original message is:\n\n";
-            users_msg_size = sizeof(users_msg_subbed_wchar) / sizeof(users_msg_subbed_wchar[0]);
-            Substitute(users_msg, users_msg_subbed_wchar, users_msg_size, key_symbols_shuffled_wchar, alphabet_plain);
+            Substitute(users_msg, users_msg_subbed_wchar, max_msg_size, key_symbols_shuffled_wchar, alphabet_plain);
             cout << users_msg << "\n";
         }
         
@@ -136,12 +126,10 @@ int main()
             
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         users_msg = "";
-        for(size_t i{}; i < users_msg_size; ++i)
+        for(size_t i{}; i < max_msg_size; ++i)
         {
-            users_msg_subbed_wchar[i] = 0;
             users_msg_subbed_unicode[i] = 0;
         }
-        users_msg_size = 0;
         system("cls");
     }
 }
@@ -150,22 +138,15 @@ int main()
 void KeyShuffled(int a_plain_size)
 {
     for(int i{}; i < a_plain_size; ++i)
-        key_symbols_shuffled_wchar[i] = key_symbols_original_wchar[i];
-
+        key_symbols_shuffled_unicode[i] = key_symbols_original_unicode[i];
     srand(time(0));
-    random_shuffle(key_symbols_shuffled_wchar, key_symbols_shuffled_wchar + a_plain_size);
-
-    for (int i = 0; i < a_plain_size; ++i)
-        for(int j{}; j < a_plain_size; ++j)
-            if(key_symbols_shuffled_wchar[j] == key_symbols_original_wchar[i])
-                key_symbols_shuffled_unicode[j] = key_symbols_original_unicode[i];
+    random_shuffle(key_symbols_shuffled_unicode, key_symbols_shuffled_unicode + a_plain_size);
 }
 
 void Substitute(string &msg,  wchar_t msg_Subbed[], size_t msg_Subbed_size, wchar_t k_shuffled[], string a_plain)
 {
     if(msg.size() == 0)
     {
-        //size_t users_msg_size = sizeof(users_msg_subbed_wchar) / sizeof(users_msg_subbed_wchar[0]);
         for(size_t i{}; i < msg_Subbed_size; ++i)
         {
             for(size_t j{}; j < a_plain.size(); ++j)
@@ -183,14 +164,14 @@ void Substitute(string &msg,  wchar_t msg_Subbed[], size_t msg_Subbed_size, wcha
         {
             if(a_plain.find(msg.at(i)) != string::npos)
             {
-                users_msg_subbed_wchar[i] = k_shuffled[a_plain.find(msg.at(i))];
+                msg_Subbed[i] = k_shuffled[a_plain.find(msg.at(i))];
             }
         }
     }
 
 }
 
-void BrowseToFile(string usr_msg_plain, string a_plain, wchar_t a_shuff[], bool IO)
+void BrowseToFile(string usr_msg_plain, string a_plain, bool IO)
 {
     OPENFILENAME ofn;           // common dialog box structure
     TCHAR szFile[260] = { 0 };  // if using TCHAR macros
@@ -259,12 +240,11 @@ void BrowseToFile(string usr_msg_plain, string a_plain, wchar_t a_shuff[], bool 
             for(size_t i{}; i < a_plain.size()-2; ++i)
                 outp_obj << (char)a_plain.at(i);
             outp_obj << "\\t\\n ]\n\n";
-            outp_obj << "Key (symbols):\t\t[ " << a_shuff << " ]\n\n\n\n";
+            outp_obj << "Key (symbols):\t\t[ " << key_symbols_shuffled_unicode << " ]\n\n\n\n";
             outp_obj << "\t\t\t\t\t[\tMESSAGE\t\t]\n\n";
-            Substitute(usr_msg_plain, users_msg_subbed_wchar, 0, a_shuff, a_plain);
             for(size_t i{}; i < usr_msg_plain.size(); ++i)
             {
-                outp_obj << users_msg_subbed_wchar[i];
+               outp_obj << users_msg_subbed_unicode[i];
                 if(i != 0 && i % 100 == 0)
                     outp_obj << "\n";
             }                
