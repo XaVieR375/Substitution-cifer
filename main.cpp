2ef23eb328ed9f96fad6c8f2b22d9c0d243d8810
XaVieR375 <67128892+XaVieR375@users.noreply.github.com>
2022-05-11 23:05:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ wchar_t key_symbols_original_unicode[]
     0x3A3, 0x3C3, 0xB5, 0x3C4, 0x3A6, 0x398, 0x3A9, 0x3B4, 0x221E, 0x3C6, 0x3B5, 0x2229, 0x2261,
     0xB1, 0x2265, 0x2264, 0x2320, 0x2321, 0xF7, 0x2248, 0xB0, 0x207F, 0xB2
 }; // C syle string with 88 chars(symbols).    Unicode conversion source / Code table values:    https://r12a.github.io/app-conversion/
-const int max_msg_size{5000};
+const int max_msg_size{50000};
 wchar_t users_msg_subbed_unicode[max_msg_size];
 
 void KeyShuffled();     // https://www.geeksforgeeks.org/shuffle-an-array-using-stl-in-c
